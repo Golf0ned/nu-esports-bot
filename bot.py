0e0f9a91fef051607131b8458144ff912f779ab0
Golf0ned <benjaminye.email@gmail.com>
2025-01-20 02:43:50 -0600

    [bot] remove test function

--- bot.py
+++ bot.py
@@ -23,22 +23,6 @@ async def on_ready():
     print(f'Ready!')
     print(f'--------------------------------------------------')
 
-
-"""
-Example command, for Ben reference:
-
-Creates a command named "test" that responds with "This is a test command" when called.
-
-@bot.command(description="Test command")
-async def test(ctx):
-    await ctx.respond("This is a test command")
-
-"""
-
-@bot.slash_command(description="Test command", guild_ids=[GUILD_ID])
-async def test_main(ctx):
-    await ctx.respond("This is a test command")
-
 for cog in cogs_list:
     bot.load_extension(f'cogs.{cog}')
     print(f'Loaded cog: {cog}')

e0cd8fbdff834cb5761d73084cf44b744af7cad1
Golf0ned <benjaminye.email@gmail.com>
2025-01-20 04:26:09 -0600

    [valorant] random custom flags

--- cogs/valorant.py
+++ cogs/valorant.py
@@ -10,88 +10,91 @@ dotenv.load_dotenv()
 GUILD_ID = os.getenv('GUILD_ID')
 
 
-AGENTS = [
-    'Brimstone',
-    'Viper',
-    'Omen',
-    'Cypher',
-    'Sova',
-    'Sage',
-    'Phoenix',
-    'Jett',
-    'Raze',
-    'Breach',
-    'Reyna',
-    'Killjoy',
-    'Skye',
-    'Yoru',
-    'Astra',
-    'KAY-O',
-    'Chamber',
-    'Neon',
-    'Fade',
-    'Harbor',
-    'Gekko',
-    'Deadlock',
-    'Iso',
-    'Clove',
-    'Vyse',
-    'Tejo',
-]
-
-AGENTS_GUARANTEED_SMOKE = [0, 2, 14, 19, 23] # say no to solo viper
-
-MAPS = [
-    'Bind',
-    'Haven',
-    'Split',
-    'Ascent',
-    'Icebox',
-    'Breeze',
-    'Fracture',
-    'Pearl',
-    'Lotus',
-    'Sunset',
-    'Abyss',
-]
-
-MAPS_IN_ROTATION = [0, 1, 2, 6, 7, 8, 10]
-
-
 class Valorant(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
 
-    def random_map(self):
-        # TODO: Implement map flags:
-        #   - Active rotation
-        #   - Just newest map
-        #   - TDM
-        return random.choice(MAPS)
+    def random_map(self, flags):
+        maps = [
+            'Bind',
+            'Haven',
+            'Split',
+            'Ascent',
+            'Icebox',
+            'Breeze',
+            'Fracture',
+            'Pearl',
+            'Lotus',
+            'Sunset',
+            'Abyss',
+        ]
 
-    def random_team(self):
-        # TODO: Implement team flags
-        #   - Guaranteed smokes
-        #   - Role-balanced
-        return random.sample(AGENTS, 5)
+        maps_active = [0, 1, 2, 6, 7, 8, 10]
 
+        match flags:
+            case 'active':
+                return maps[random.choice(maps_active)]
+            case 'newest':
+                return maps[-1]
+            case _:
+                return random.choice(maps)
+
+    def random_team(self, flags):
+        # Note that Viper's grouped with the sentinels.
+        # From an agent pick perspective, it leads to better random teams.
+        # (we do not endorse solo viper!)
+        controllers = ['Brimstone', 'Omen', 'Astra', 'Harbor', 'Clove']
+        duelists = ['Phoenix', 'Jett', 'Raze', 'Reyna', 'Yoru', 'Neon', 'Iso']
+        initiators = ['Sova', 'Breach', 'Skye', 'KAY/O', 'Fade', 'Gekko', 'Tejo']
+        sentinels = ['Viper', 'Cypher', 'Sage', 'Killjoy', 'Chamber', 'Deadlock', 'Vyse']
+
+        match flags:
+            case 'role-balanced':
+                team = [
+                    controllers.pop(random.randrange(len(controllers))),
+                    duelists.pop(random.randrange(len(duelists))),
+                    initiators.pop(random.randrange(len(initiators))),
+                    sentinels.pop(random.randrange(len(sentinels))),
+                ]
+                all = controllers + duelists + initiators + sentinels
+                team.append(random.choice(all))
+                random.shuffle(team)
+                return team
+            case _:
+                all = controllers + duelists + initiators + sentinels
+                return random.sample(all, 5)
+        
     valorant = discord.SlashCommandGroup('valorant', 'Valorant-related utils')
 
     @valorant.command(description='Generates a randomized Valorant lobby', guild_ids=[GUILD_ID])
-    async def random_lobby(self, ctx):
-        map = self.random_map()
-        attackers = self.random_team()
-        defenders = self.random_team()
+    async def random_lobby(
+            self,
+            ctx,
+            map_flags: discord.Option(
+                str,
+                name='maps',
+                choices=['active', 'newest', 'all'],
+                default='all',
+            ),
+            team_flags: discord.Option(str,
+                                       name='teams',
+                                       choices=['role-balanced', 'random'],
+                                       default='role-balanced',
+            ),
+    ):
+        map = self.random_map(map_flags)
+        attackers = self.random_team(team_flags)
+        defenders = self.random_team(team_flags)
 
         embed = discord.Embed(
             title='Valorant Randomized Lobby',
             color=discord.Color.from_rgb(78, 42, 132),
         )
 
-        embed.add_field(name='Map', value=map)
-        embed.add_field(name='Attackers', value='\n'.join(attackers), inline=True)
-        embed.add_field(name='Defenders', value='\n'.join(defenders), inline=True)
+        embed.add_field(name=':map: Map', value=map, inline=False)
+        embed.add_field(name=':red_square: Attackers', value='\n'.join(attackers), inline=True)
+        embed.add_field(name=':blue_square: Defenders', value='\n'.join(defenders), inline=True)
 
         await ctx.respond('', embed=embed)
 

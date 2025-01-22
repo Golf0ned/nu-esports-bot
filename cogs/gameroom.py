d8e41dbc5e515f5b38d7899cebba49b0bc143024
Golf0ned <benjaminye.email@gmail.com>
2025-01-22 16:42:18 -0600

    [gameroom] revamp gameroom hours

--- cogs/gameroom.py
+++ cogs/gameroom.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 
 import discord
@@ -18,23 +19,41 @@ class Gameroom(commands.Cog):
 
     @gameroom.command(name='hours', description='Lists current game room hours', guild_ids=[GUILD_ID])
     async def hours(self, ctx):
-        hours = {
-            'Sunday': '2:30 PM - 11:00 PM',
-            'Monday': '2:30 PM - 11:00 PM',
-            'Tuesday': '2:30 PM - 11:00 PM',
-            'Wednesday': '2:30 PM - 11:00 PM',
-            'Thursday': '2:30 PM - 11:00 PM',
-            'Friday': '12:30 PM - 11:00 PM',
-            'Saturday': '12:30 PM - 11:00 PM',
-        }
+        # Monday to Sunday
+        default_hours = [
+            '2:30PM - 11:00PM',
+            '2:30PM - 11:00PM',
+            '2:30PM - 11:00PM',
+            '2:30PM - 11:00PM',
+            '12:30PM - 11:00PM',
+            '12:30PM - 11:00PM',
+            '2:30PM - 11:00PM',
+        ]
+        
+        # List of days the game room is closed:
+        # (yes, we're adding by hand)
+        # - MLK Jr Day
+        days_closed = [
+            datetime.date(2025, 1, 20)
+        ]
+
+        today = datetime.date.today()
+        start = today - datetime.timedelta(days=today.weekday())
+        end = start + datetime.timedelta(days=6)
+        week = [start + datetime.timedelta(days=i) for i in range(7)]
 
         embed = discord.Embed(
             title='Game Room Hours',
             color=discord.Color.from_rgb(78, 42, 132),
         )
 
-        for day, hours in hours.items():
-            embed.add_field(name=day, value=hours, inline=False)
+        embed.add_field(name=f'Week of {start.strftime("%-m/%-d")} - {end.strftime("%-m/%-d")}', value='')
+
+        for i, day in enumerate(week):
+            value = default_hours[i] if day not in days_closed else 'Closed'
+            embed.add_field(name=day.strftime('%A'), value=value, inline=False)
+
+        embed.set_image(url='https://www.northwestern.edu/norris/arts-recreation/game-room/nexus_general_awareness-01.png')
 
         await ctx.respond('', embed=embed)
 

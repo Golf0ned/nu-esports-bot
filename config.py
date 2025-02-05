a36331f82027b4207a98a364503c8f50be3205b7
Golf0ned <benjaminye.email@gmail.com>
2025-02-04 22:45:08 -0600

    i had a revelation that double quotes are the TRUTH

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import yaml
 
 
-config_file = 'config.yaml'
-secrets_file = 'secrets.yaml'
+config_file = "config.yaml"
+secrets_file = "secrets.yaml"
 
 
 class Config:
 
-    with open(config_file, 'r') as f:
+    with open(config_file, "r") as f:
         config = yaml.safe_load(f)
 
-    with open(secrets_file, 'r') as f:
+    with open(secrets_file, "r") as f:
         secrets = yaml.safe_load(f)

96dd9793cdf85dd146ffaa815f5a1748caec88a1
hannahbanllana <162388141+hannahbanllana@users.noreply.github.com>
2025-10-08 18:55:09 -0500

    separated fun.py into separate functions per interaction

--- cogs/fun.py
+++ cogs/fun.py
@@ -14,40 +14,55 @@ class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.author == self.bot.user:
-            return
-
-        # React with random chess emoji on ping
-        if self.bot.user.mentioned_in(message):
-            if message.mention_everyone:
+    def chess(self, message):
+        @commands.Cog.listener()
+        async def on_message(self, message):
+            if message.author == self.bot.user:
                 return
-            chess_emojis = config.config["fun"]["chess_emojis"]
-            emoji, id = random.choice(list(chess_emojis.items()))
-            await message.add_reaction(f"<:{emoji}:{id}>")
+            
+            if self.bot.user.mentioned_in(message):
+                if message.mention_everyone:
+                    return
+                chess_emojis = config.config["fun"]["chess_emojis"]
+                emoji, id = random.choice(list(chess_emojis.items()))
+                await message.add_reaction(f"<:{emoji}:{id}>")
 
-        # Affirm the glory of osu!
-        lower_content = message.content.lower()
-        if "i love osu" in lower_content:
-            await message.reply("Osu 😻")
+    def i_love_osu(self, message):
+        @commands.Cog.listener()
+        async def on_message(self, message):
+            if message.author == self.bot.user:
+                return
+            
+            lower_content = message.content.lower()
+            if "i love osu" in lower_content:
+                await message.reply("Osu 😻")
 
-        if random.randint(1,100) <= 10 and "oh lord" in lower_content:
-            await message.reply('https://www.youtube.com/watch?v=YsoP6bjADic')
+    def oh_lord(self, message):
+        @commands.Cog.listener()
+        async def on_message(self, message):
+            if message.author == self.bot.user:
+                return
+            
+            lower_content = message.content.lower()
+            if random.randint(1,100) <= 10 and "oh lord" in lower_content:
+                await message.reply('https://www.youtube.com/watch?v=YsoP6bjADic')
 
-        # User-specific reactions
-        special_users = config.config["fun"]["special_users"]
-        if random.randint(1, 100) <= 15 and message.author.id in special_users:
-            emoji_set = random.choice(special_users[message.author.id])
-            if isinstance(emoji_set, list):
-                # List of reactions
-                for emoji in emoji_set:
-                    await message.add_reaction(emoji)
-            else:
-                # Single reaction
-                await message.add_reaction(emoji_set)
-
-
+    def special_user_reacts(self, message):
+        @commands.Cog.listener()
+        async def on_message(self, message):
+            if message.author == self.bot.user:
+                return
+            
+            special_users = config.config["fun"]["special_users"]
+            if random.randint(1, 100) <= 15 and message.author.id in special_users:
+                emoji_set = random.choice(special_users[message.author.id])
+                if isinstance(emoji_set, list):
+                    # List of reactions
+                    for emoji in emoji_set:
+                        await message.add_reaction(emoji)
+                else:
+                    # Single reaction
+                    await message.add_reaction(emoji_set)
 
 def setup(bot):
     bot.add_cog(Fun(bot))

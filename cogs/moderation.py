e049bd382e5943e88175d944bb624e868067a305
Lii-lac <alexkosowski2028@u.northwestern.edu>
2026-05-08 03:58:43 -0500

    added more options to lock and unlock

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -28,33 +28,35 @@ class Moderation(commands.Cog):
 
     @discord.slash_command(
         name="lock",
-        description="Locks a channel",
+        description="Locks a channel (hiding optional). /Unlock to undo",
         guild_ids=[GUILD_ID]
     )
     @default_permissions(manage_channels=True)
-    async def lock(self, ctx: discord.ApplicationContext):
+    async def lock(self, ctx: discord.ApplicationContext, hide: bool=True):
         if not ctx.channel.permissions_for(ctx.author).manage_channels:
             await ctx.respond("You do not have permission to use this command.", ephemeral=True)
             return
         
         overwrite = ctx.channel.overwrites_for(ctx.guild.default_role)
         overwrite.send_messages = False 
+        if hide: overwrite.view_channel = False
         await ctx.channel.set_permissions(ctx.guild.default_role, overwrite=overwrite)
         await ctx.respond("🔒 Channel locked.")
 
     @discord.slash_command(
         name="unlock",
-        description="Locks a channel",
+        description="Unlocks a channel (unhiding optional)",
         guild_ids=[GUILD_ID]
     )
     @default_permissions(manage_channels=True)
-    async def unlock(self, ctx: discord.ApplicationContext):
+    async def unlock(self, ctx: discord.ApplicationContext, unhide: bool=True):
         if not ctx.channel.permissions_for(ctx.author).manage_channels:
             await ctx.respond("You do not have permission to use this command.", ephemeral=True)
             return
         
         overwrite = ctx.channel.overwrites_for(ctx.guild.default_role)
-        overwrite.send_messages = True 
+        overwrite.send_messages = True
+        if unhide: overwrite.view_channel = True
         await ctx.channel.set_permissions(ctx.guild.default_role, overwrite=overwrite)
         await ctx.respond("🔓 Channel unlocked.", ephemeral=True)
 
